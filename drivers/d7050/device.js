166d16ebcf112275a01f081c47f1daf4b9958bb3
abaretta <abaretta@protonmail.com>
2019-01-04 19:35:06 +0100

    Bug fix

--- drivers/d7050/device.js
+++ drivers/d7050/device.js
@@ -59,31 +59,31 @@ class D7050Device extends Homey.Device {
         // console.dir(devices); // for debugging
 
         // register capability listeners
+
+        // capability listeners for the mobile interface
         this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
         this.registerCapabilityListener('volume_set', this.onCapabilityVolumeSet.bind(this));
-        this.registerCapabilityListener('volume_mute', this.onCapabilityVolumeMute.bind(this));
-        this.registerCapabilityListener('volume_up', this.onCapabilityVolumeUp.bind(this));
-        this.registerCapabilityListener('volume_down', this.onCapabilityVolumeDown.bind(this));
         this.registerCapabilityListener('input_selected', this.onCapabilityinput_selected.bind(this));
-        this.registerCapabilityListener('volume_mute', this.onCapabilityMute.bind(this));
+        // Would be nice to be able to have the mute function in the mobile interface as well
+        //this.registerCapabilityListener('volume_mute', this.onCapabilityVolumeMute.bind(this));
 
         // flow conditions
         this._conditionOnoff = new Homey.FlowCardCondition('onoff').register()
-            .registerRunListener((args, state) => {
+            .registerRunListener(() => {
                 var result = (this.getCapabilityValue('onoff'));
                 return Promise.resolve(result);
             })
 
         this._conditionMute = new Homey.FlowCardCondition('volume_mute').register()
-            .registerRunListener((args, state) => {
+            .registerRunListener(() => {
                 var result = (this.getCapabilityValue('volume_mute'));
                 return Promise.resolve(result);
             })
 
         this._conditionInput_selected = new Homey.FlowCardCondition('input_selected').register()
-            .registerRunListener((args, state) => {
+            .registerRunListener((args) => {
                 var selected_input = args.input.inputName;
-                if (selected_input == this.getCapabilityValue('input_selected')) {
+                if (selected_input === this.getCapabilityValue('input_selected')) {
                     this.log("Selected source equals current source: " + selected_input);
                     return Promise.resolve(true);
                 } else {
@@ -94,7 +94,7 @@ class D7050Device extends Homey.Device {
 
         this._conditionInput_selected
             .getArgument('input')
-            .registerAutocompleteListener((query, args) => {
+            .registerAutocompleteListener((query) => {
                 var items = this.searchForInputsByValue(query);
                 return Promise.resolve(items);
             });
@@ -102,15 +102,14 @@ class D7050Device extends Homey.Device {
         // register flow card actions
         let powerOnAction = new Homey.FlowCardAction('powerOn');
         powerOnAction
-            .register().registerRunListener((args, state) => {
-                // this.log("Flow card action powerOn args.zone: "+JSON.stringify(args.zone));
+            .register().registerRunListener((args) => {
                 this.powerOn(args.device);
                 return Promise.resolve(true);
             });
 
         let powerOffAction = new Homey.FlowCardAction('powerOff');
         powerOffAction
-            .register().registerRunListener((args, state) => {
+            .register().registerRunListener((args) => {
                 this.log("Flow card action powerOff args " + args);
                 this.powerOff(args.device);
                 return Promise.resolve(true);
@@ -118,57 +117,57 @@ class D7050Device extends Homey.Device {
 
         let muteAction = new Homey.FlowCardAction('mute');
         muteAction
-            .register().registerRunListener((args, state) => {
-                this.log("Flow card action mute args " + args);
+            .register().registerRunListener((args) => {
+                this.log("Flow card action mute args " + args.device);
                 this.onActionMute(args.device);
                 return Promise.resolve(true);
             });
 
         let unMuteAction = new Homey.FlowCardAction('unMute');
         unMuteAction
-            .register().registerRunListener((args, state) => {
-                this.log("Flow card action unMute args " + args);
+            .register().registerRunListener((args) => {
+                this.log("Flow card action unMute ");
                 this.onActionUnMute(args.device);
                 return Promise.resolve(true);
             });
 
         let toggleMuteAction = new Homey.FlowCardAction('toggleMute');
         toggleMuteAction
-            .register().registerRunListener((args, state) => {
-                this.log("Flow card action toggleMute args " + args);
+            .register().registerRunListener((args) => {
+                this.log("Flow card action toggleMute ");
                 this.onActionToggleMute(args.device);
                 return Promise.resolve(true);
             });
 
         let setVolumeAction = new Homey.FlowCardAction('setVolume');
         setVolumeAction
-            .register().registerRunListener((args, state) => {
-                this.log(" setVolume volume " + args.volume);
-                this.onActionSetVolume(args.device, args.volume);
+            .register().registerRunListener((args) => {
+                this.log("Flow card action setVolume args ");
+                this.onActionSetVolume(this, args.volume);
                 return Promise.resolve(true);
             });
 
         let volumeUpAction = new Homey.FlowCardAction('volumeUp');
         volumeUpAction
-            .register().registerRunListener((args, state) => {
-                this.log(" setVolume volume up " + args.volume);
-                this.onActionVolumeUp(args.device, args.volume);
+            .register().registerRunListener((args) => {
+                this.log("Flow card action volumeUp args ");
+                this.onActionVolumeUp(this, args.volume);
                 return Promise.resolve(true);
             });
 
         let volumeDownAction = new Homey.FlowCardAction('volumeDown');
         volumeDownAction
-            .register().registerRunListener((args, state) => {
-                this.log(" setVolume volume Down " + args.volume);
-                this.onActionVolumeDown(args.device, args.volume);
+            .register().registerRunListener((args) => {
+                this.log(" setVolume volume Down ");
+                this.onActionVolumeDown(this, args.volume);
                 return Promise.resolve(true);
             });
 
         let changeInputAction = new Homey.FlowCardAction('input_selected');
         changeInputAction
             .register()
-            .registerRunListener((args, state) => {
-                this.log("Flow card action changeInput args " + args);
+            .registerRunListener((args) => {
+                this.log("Flow card action changeInput args " + args.input.inputName);
                 this.log(" changeInput input " + args.input.inputName);
                 this.onActioninput_selected(args.device, args.input.inputName);
                 return Promise.resolve(true);
@@ -176,7 +175,7 @@ class D7050Device extends Homey.Device {
 
         changeInputAction
             .getArgument('input')
-            .registerAutocompleteListener((query, args) => {
+            .registerAutocompleteListener((query) => {
                 var items = this.searchForInputsByValue(query);
                 return Promise.resolve(items);
             });
@@ -233,24 +232,6 @@ class D7050Device extends Homey.Device {
         callback(null);
     }
 
-    onCapabilityVolumeUp(value, opts, callback) {
-        this.log("Capability called: volume_up");
-        this.volumeUp(this, value);
-        callback(null);
-    }
-
-    onCapabilityVolumeDown(value, opts, callback) {
-        this.log("Capability called: volume_down");
-        this.volumeDown(this, value);
-        callback(null);
-    }
-
-    onCapabilityMute(value, opts, callback) {
-        this.log("Capability called: mute");
-        this.mute(this);
-        callback(null);
-    }
-
     onCapabilityinput_selected(value, opts, callback) {
         this.log("Capability called: input_selected. Value: " + value);
         this.changeInputSource(this, value);
@@ -281,17 +262,25 @@ class D7050Device extends Homey.Device {
 
     onActionVolumeUp(device, step) {
         this.log("Action called: setVolumeUp");
-        device.volumeUp(device, step);
+        var currentVolume = this.getCapabilityValue('volume_set');
+        this.log("currentVolume: " + currentVolume);
+        this.log("typeof currentVolume: " + typeof (currentVolume));
+        var targetVolume = currentVolume + (step / 100);
+        targetVolume = targetVolume.toFixed(2);
+        this.log("volumeUp targetVolume: " + targetVolume);
+        this.log("setVolumeUp targetVolume type: " + typeof (targetVolume));
+        device.setVolume(device, targetVolume);
     }
 
     onActionVolumeDown(device, step) {
         this.log("Action called: setVolumeDown");
-        device.volumeDown(device, step);
-    }
-
-    onActionSetVolumeStep(device, volumeChange) {
-        this.log("Action called: setVolumeStep");
-        device.setVolumeStep(device, volumeChange);
+        var currentVolume = this.getCapabilityValue('volume_set');
+        device.log("currentVolume: " + currentVolume);
+        var targetVolume = currentVolume - (step / 100);
+        targetVolume = targetVolume.toFixed(2);
+        device.log("volumeDown targetVolume: " + targetVolume);
+        device.setVolume(device, targetVolume);
+        this.log("setVolumeDown targetVolume type: " + typeof (targetVolume));
     }
 
     onActioninput_selected(device, input) {
@@ -317,6 +306,7 @@ class D7050Device extends Homey.Device {
     }
 
     // NB: there is a bug in the D7050 that prevents auto-shutoff when spotify was used in some cases
+    // in the meantime the Spotify connect feature has been disabled... Need to re-test
     autoShutOffOff(device) {
         var command = '00010208000001020208';
         device.sendCommand(device, command, 0);
@@ -342,32 +332,27 @@ class D7050Device extends Homey.Device {
 
     mute(device) {
         var command = '0001020a01';
-        device.sendCommand(device, command, 0);
         this.setCapabilityValue('volume_mute', true)
+        device.sendCommand(device, command, 0);
     }
 
     unMute(device) {
         var command = '0001020a00';
-        device.sendCommand(device, command, 0);
         this.setCapabilityValue('volume_mute', false)
+        device.sendCommand(device, command, 0);
     }
 
     toggleMute(device) {
         device.log("Volume_mute: " + this.getCapabilityValue('volume_mute'))
         if (this.getCapabilityValue('volume_mute') == true) {
-            var command = '0001020a00';
-            this.setCapabilityValue('volume_mute', false)
+            device.unMute(device)
         } else {
-            var command = '0001020a01';
-            this.setCapabilityValue('volume_mute', true)
+            device.mute(device)
         }
-        device.sendCommand(device, command, 0);
     }
 
     setVolume(device, targetVolume) {
-        this.log("Device in setVolume: ")
         // volume ranges from 0 (-90dB) to 200 (10dB) on the D7050. Add 2 to compensate for max of 99 in GUI
-        this.setCapabilityValue('volume_set', targetVolume);
         var Volume_hex = (200 * parseFloat(targetVolume).toFixed(2)).toString(16);
         if (Volume_hex.length < 2) Volume_hex = '0' + Volume_hex;
         if (Volume_hex.length > 2) Volume_hex = Volume_hex.slice(0, 2);
@@ -376,6 +361,10 @@ class D7050Device extends Homey.Device {
         var command = '00010204' + Volume_hex;
         this.log("setVolume: " + targetVolume + " command: " + command);
         device.sendCommand(device, command, 0);
+        this.log("setVolume targetVolume type before: " + typeof (targetVolume));
+        targetVolume = parseFloat(targetVolume);
+        this.log("setVolume targetVolume type after: " + typeof (targetVolume));
+        device.setCapabilityValue('volume_set', targetVolume);
     }
 
     volumeUp(device, step) {
@@ -405,14 +394,11 @@ class D7050Device extends Homey.Device {
 
     async sendCommand(device, command, poll) {
         try {
-            //let hostIP = device;
             let id = this.getData().id;
-            this.log("hostIP: " + id);
-            //this.log("id: " + id);
             let client = devices[id].client;
             devices[id].receivedData = "";
             // check if client (net.Socket) already exists, if not then open one.
-            if ((typeof (client) === 'undefined') || (typeof (client.destroyed) != 'boolean') || (client.destroyed == true)) {
+            if ((typeof (client) === 'undefined') || (typeof (client.destroyed) != 'boolean') || (client.destroyed === true)) {
                 this.log("Opening new net.Socket to " + id + ":" + IPPort);
                 client = new net.Socket();
                 client.connect(IPPort, id);
@@ -431,7 +417,11 @@ class D7050Device extends Homey.Device {
                         client.destroy();
                     }
                 })
-
+                // Closing connection after time-out to free up port
+                setTimeout(() => {
+                    client.end();
+                    //this.log("Closing connection");
+                }, 1500);
                 devices[id].client = client;
             }
             this.log("Writing command to client: " + command)
@@ -440,50 +430,39 @@ class D7050Device extends Homey.Device {
             // add handler for any response or other data coming from the device
             if (poll == 1) {
                 await this.readStream(client, 20).then((value) => {
-                        this.log("readStream result value: " + value.toString('hex'))
                         devices[id].receivedData = value.toString('hex');
-                        //return devices[hostIP].receivedData;
                     })
                     .catch(err => {
                         this.log(err)
                     });
             }
-            this.log("Poll: " + poll)
-            this.log("devices[id].receivedData type: " + typeof (devices[id].receivedData))
-            this.log("ReceivedData: " + devices[id].receivedData);
-            if ((devices[id].receivedData != 'undefined') && (typeof (devices[id].receivedData) === 'string')) {
+            if ((devices[id].receivedData != 'undefined') && (typeof (devices[id].receivedData) == 'string')) {
                 reachable = 0;
                 this.setAvailable();
-                this.log("ReceivedData is not undefined: " + devices[id].receivedData)
+                this.log("Poll: " + poll + " ReceivedData: " + devices[id].receivedData);
                 return devices[id].receivedData
             } else {
                 this.log("ReceivedData undefined: " + devices[id].receivedData)
                 return 0;
             }
+
         } catch (err) {
-            this.log(err)
+            this.log("sendCommand error: " + err)
         }
     }
 
     readData(receivedData) {
         try {
-            this.log("ReceivedData passed to readData: " + receivedData)
-            this.log("ReceivedData type: " + typeof (receivedData))
-
-            if ((typeof (receivedData) === 'string') && (receivedData.length >= 40)) {
+            if ((typeof (receivedData) == 'string') && (receivedData.length >= 40)) {
                 this.log("NAD D7050 app - receivedData: " + receivedData);
                 reachable = 0;
                 this.setAvailable();
-
                 var onoffstring = receivedData.slice(0, 10);
-                this.log("onoffstring: " + onoffstring);
 
                 if (onoffstring.indexOf('0001020901') >= 0) {
                     var onoffState = true;
-                    this.log("NAD D7050 app - on: " + onoffstring);
                 } else if (onoffstring.indexOf('0001020900') >= 0) {
                     var onoffState = false;
-                    this.log("NAD D7050 app - in standby mode: " + onoffstring);
                     // in standby mode the amplifier can be switched on using the app, so it is still available
                 } else {
                     this.log("NAD D7050 app - unintelligible response ");
@@ -491,24 +470,18 @@ class D7050Device extends Homey.Device {
                     return 0;
                 }
                 var input_selected = receivedData.slice(19, 20);
-                this.log("input_selected: " + input_selected);
                 var volumeState_hex = receivedData.slice(28, 30);
-                //this.log("Volume_hex:" + volumeState_hex)
                 var volume = Math.round(Number('0x' + volumeState_hex).toString(10));
-                this.log("volume: " + volume);
                 // Convert volume 0 - 200 to percentage for volume_set
                 var volume_percent = parseFloat(volume / 200).toFixed(2);
-                //this.log("Volume_percent:" + parseFloat(volume_percent.toFixed(2)))
-                //this.log("Volume_set:" + this.getCapabilityValue('volume_set'))
                 var mutestring = receivedData.slice(30, 40);
-                this.log("mutestring: " + mutestring);
                 if (mutestring.indexOf('0001020a01') >= 0) {
-                    this.log('NAD D7050 app - Amp is muted: ' + mutestring);
                     var muteState = true;
                 } else {
-                    this.log('NAD D7050 app - Amp is not muted: ' + mutestring);
                     var muteState = false;
                 }
+                this.log("Amp on: " + onoffState + " Source: " + input_selected + " Vol: " + volume + "/" + volume_percent + " Mute: " + muteState)
+
                 if (this.getCapabilityValue('onoff') != onoffState) {
                     this.setCapabilityValue('onoff', onoffState);
                 }
@@ -516,7 +489,7 @@ class D7050Device extends Homey.Device {
                     this.setCapabilityValue('input_selected', input_selected);
                 }
                 if (this.getCapabilityValue('volume_set') != volume_percent) {
-                    this.setCapabilityValue('volume_set', volume_percent);
+                    this.setCapabilityValue('volume_set', parseFloat(volume_percent));
                 }
                 if (this.getCapabilityValue('volume_mute') != muteState) {
                     this.setCapabilityValue('volume_mute', muteState);
@@ -524,14 +497,17 @@ class D7050Device extends Homey.Device {
                 return 1;
             }
         } catch (err) {
-            //  return 0;
-            this.log(err)
+            this.log("readData error: " + err)
         }
     }
 
     async executeAsyncTask(id, command, poll) {
-        const receivedData = await this.sendCommand(id, command, poll)
-        return await this.readData(receivedData)
+        try {
+            const receivedData = await this.sendCommand(id, command, poll)
+            return await this.readData(receivedData)
+        } catch (err) {
+            this.log("AsyncTask error: " + err)
+        }
     }
 
     pollDevice(interval) {
